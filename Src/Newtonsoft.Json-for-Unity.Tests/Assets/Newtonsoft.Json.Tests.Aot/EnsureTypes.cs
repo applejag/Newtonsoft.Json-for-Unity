aab75982c64acaab4fbebe799d7e97423dce71c1
kalle (jag) <kalle.jillheden@gmail.com>
2019-07-16 23:33:45 +0200

    Disabled tests for expando objects & reordered ensuretypes into groups

--- Src/Newtonsoft.Json-for-Unity.Tests/Assets/Newtonsoft.Json.Tests.Aot/EnsureTypes.cs
+++ Src/Newtonsoft.Json-for-Unity.Tests/Assets/Newtonsoft.Json.Tests.Aot/EnsureTypes.cs
@@ -13,25 +13,25 @@ namespace Newtonsoft.Json.Tests.Aot
         [SetUp]
         public void TestInitialize()
         {
-            AotHelper.EnsureList<DateTime>(); // used in DeserializeDateFormatString
-            AotHelper.EnsureList<Guid>(); // used in BsonReaderTests
-            AotHelper.EnsureList<KeyValuePair<string, int>>(); // used in KeyValuePairConverterTests
-            AotHelper.EnsureList<KeyValuePair<string, string>>(); // used in Issue1322
+            AotHelper.EnsureList<bool>(); // used in JsonSerializerTest.DeserializeBooleans
+            AotHelper.EnsureList<bool?>(); // used in JsonSerializerTest.DeserializeNullableBooleans
             AotHelper.EnsureList<int>(); // used in ContractResolverTests.ListInterfaceDefaultCreator
             AotHelper.EnsureList<int?>(); // used in JsonSerializerCollectionsTests.DeserializeIEnumerableFromConstructor
-            AotHelper.EnsureList<KeyValuePair<string, IList<string>>>(); // used in JsonSerializerCollectionsTests.DeserializeKeyValuePairArray
-            AotHelper.EnsureList<KeyValuePair<string, IList<string>>?>(); // used in JsonSerializerCollectionsTests.DeserializeNullableKeyValuePairArray
+            AotHelper.EnsureList<long>(); // used in JsonSerializerTest.ReadTooLargeInteger
+            AotHelper.EnsureList<float>(); // used in JsonSerializerTest.ReadStringFloatingPointSymbols
+            AotHelper.EnsureList<double>(); // used in JsonSerializerTest.ReadStringFloatingPointSymbols
+            AotHelper.EnsureList<double?>(); // used in JsonSerializerTest.DeserializeNullableArray
             AotHelper.EnsureList<decimal>(); // used in JsonSerializerTest.CommentTestClassTest
-            AotHelper.EnsureList<bool>(); // used in JsonSerializerTest.DeserializeBooleans
+            AotHelper.EnsureList<BigInteger>(); // used in JsonSerializerTest.ReadTooLargeInteger
+            AotHelper.EnsureList<Guid>(); // used in BsonReaderTests
+            AotHelper.EnsureList<DateTime>(); // used in DeserializeDateFormatString
+            AotHelper.EnsureList<DateTimeOffset?>(); // used in JsonSerializerTest.ReadForTypeHackFixDateTimeOffset
+            AotHelper.EnsureList<KeyValuePair<string, IList<string>>?>(); // used in JsonSerializerCollectionsTests.DeserializeNullableKeyValuePairArray
+            AotHelper.EnsureList<KeyValuePair<string, IList<string>>>(); // used in JsonSerializerCollectionsTests.DeserializeKeyValuePairArray
+            AotHelper.EnsureList<KeyValuePair<string, int>>(); // used in KeyValuePairConverterTests
+            AotHelper.EnsureList<KeyValuePair<string, string>>(); // used in Issue1322
             AotHelper.EnsureDictionary<string, decimal>(); // used in JsonSerializerTest.DeserializeDecimalDictionaryExact
             AotHelper.EnsureDictionary<string, int>(); // used in JsonSerializerCollectionsTests.SerializeCustomReadOnlyDictionary
-            AotHelper.EnsureList<double?>(); // used in JsonSerializerTest.DeserializeNullableArray
-            AotHelper.EnsureList<double>(); // used in JsonSerializerTest.ReadStringFloatingPointSymbols
-            AotHelper.EnsureList<bool?>(); // used in JsonSerializerTest.DeserializeNullableBooleans
-            AotHelper.EnsureList<DateTimeOffset?>(); // used in JsonSerializerTest.ReadForTypeHackFixDateTimeOffset
-            AotHelper.EnsureList<float>(); // used in JsonSerializerTest.ReadStringFloatingPointSymbols
-            AotHelper.EnsureList<BigInteger>(); // used in JsonSerializerTest.ReadTooLargeInteger
-            AotHelper.EnsureList<long>(); // used in JsonSerializerTest.ReadTooLargeInteger
         }
     }
 }
