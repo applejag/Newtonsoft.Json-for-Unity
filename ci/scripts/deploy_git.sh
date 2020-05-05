31e9fe53bc09706daaad598c4b160337cf3270c7
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-05-05 22:20:43 +0200

    Updated deploy_git.sh to deploy tags as `upm/$VERSION` instead to not collide with existing tags

--- ci/scripts/deploy_git.sh
+++ ci/scripts/deploy_git.sh
@@ -12,62 +12,87 @@ PACKAGE_FOLDER="${1:-${PACKAGE_FOLDER:?'Package folder required'}}"
 : ${VERSION_RELEASE:?}
 : ${REPO_FOLDER:?}
 
-if git tag --list | egrep -q "^$VERSION$"
+TAG_UPM="upm/$VERSION"
+TAG_MASTER="$VERSION"
+
+tag_and_push() {
+    local tagName="$1"
+    local tagMessage="$2"
+
+    git tag $tagName -m "$tagMessage"
+
+    echo "Created tag: '$(git tag -l $tagName -n1))'"
+
+    if [ "${VERSION_AUTO_DEPLOY_DRY_RUN:-}" == "false" ]
+    then
+        git push --follow-tags
+    else
+        echo "RUNNING GIT PUSH DRY-RUN"
+        git push --follow-tags --dry-run
+        echo "RUNNING GIT PUSH DRY-RUN"
+    fi
+    echo
+    echo "Successfully pushed"
+}
+
+#-------------------------------------------------------------------------------
+
+if git tag --list | egrep -q "^$TAG_MASTER$"
 then
-    echo "Tag $VERSION already existed. Skipping the deployment"
-    exit 0
+    echo "Tag $TAG_MASTER already existed. Skipping the tagging on master branch"
+else
+    tag_and_push $TAG_MASTER "Json.NET $VERSION_JSON_NET, release $VERSION_RELEASE
+
+Created by CircleCI job
+Build #$CIRCLE_BUILD_NUM
+$CIRCLE_BUILD_URL"
 fi
 
-echo ">> Checking out upm branch"
-git checkout upm --force
-echo
+#-------------------------------------------------------------------------------
 
-echo ">> Replacing package"
-shopt -s dotglob
-GLOBIGNORE='.git' git rm \* -rf --ignore-unmatch
-git clean -dfx
-mv $PACKAGE_FOLDER/* $REPO_FOLDER/.
-shopt -u dotglob
-git add .
-echo
-
-echo ">> Status"
-git status --short
-STATUS="$(git status --short)"
-echo
-
-if [ -z "$STATUS" ]
+if git tag --list | egrep -q "^$TAG_UPM$"
 then
-    echo "No changes to package in UPM branch. Will not create a new commit."
+    echo "Tag $TAG_UPM already existed. Skipping the deployment to upm branch"
 else
-    git commit -m "Json.NET $VERSION_JSON_NET, release $VERSION_RELEASE
+
+    echo ">> Checking out upm branch"
+    git checkout upm --force
+    echo
+
+    echo ">> Replacing package"
+    shopt -s dotglob
+    GLOBIGNORE='.git' git rm \* -rf --ignore-unmatch
+    git clean -dfx
+    mv $PACKAGE_FOLDER/* $REPO_FOLDER/.
+    shopt -u dotglob
+    git add .
+    echo
+
+    echo ">> Status"
+    git status --short
+    STATUS="$(git status --short)"
+    echo
+
+    if [ -z "$STATUS" ]
+    then
+        echo "No changes to package in UPM branch. Will not create a new commit."
+    else
+        git commit -m "Json.NET $VERSION_JSON_NET, release $VERSION_RELEASE
 
 Based on commit $CIRCLE_SHA1
 
 Created by CircleCI job
 Build #$CIRCLE_BUILD_NUM
 $CIRCLE_BUILD_URL"
-    echo "Created commit '$(git log -n1 --format="%s")'"
-fi
-echo
+        echo "Created commit '$(git log -n1 --format="%s")'"
+    fi
+    echo
 
-git tag $VERSION -m "Json.NET $VERSION_JSON_NET, release $VERSION_RELEASE
+    tag_and_push $TAG_UPM "Json.NET $VERSION_JSON_NET, release $VERSION_RELEASE
 
 Based on commit $CIRCLE_SHA1
 
 Created by CircleCI job
 Build #$CIRCLE_BUILD_NUM
 $CIRCLE_BUILD_URL"
-
-echo "Created tag '$(git tag -l $VERSION -n1)'"
-
-if [ "${VERSION_AUTO_DEPLOY_DRY_RUN:-}" == "false" ]
-then
-    git push --follow-tags
-else
-    echo "RUNNING GIT PUSH DRY-RUN"
-    git push --follow-tags --dry-run
-    echo "RUNNING GIT PUSH DRY-RUN"
 fi
-echo
-echo "Successfully pushed"
